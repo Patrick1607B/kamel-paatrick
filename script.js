7ef983f72275e3cb3e9f9f1175633a1d9cfaf697
Patrick1607B <118965823+Patrick1607B@users.noreply.github.com>
2023-01-05 14:07:48 +0100

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("🔫");
+const cop1 = document.querySelector(".cop1")
+const cop2 = document.querySelector(".cop2")
+
+const killer1 = document.querySelector(".killer1")
+const killer2 = document.querySelector(".killer2")
+
